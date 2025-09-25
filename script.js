7956b0efe7d9860b8c2bfac16bfb9051ae1683f3
Naveen Kumar Singh <136342619+Naveen-ns0@users.noreply.github.com>
2025-09-25 19:03:02 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// Toggle dropdown menu
+const menuToggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+menuToggle.addEventListener('click', () => {
+  navLinks.classList.toggle('show');
+});
